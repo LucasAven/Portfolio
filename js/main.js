73a2bea07692163a865d86176917c0669fc39539
LUCAS FEDERICO AVENDAÑO ZARZA <lavendanozarza@alu.frlp.utn.edu.ar>
2021-12-18 18:36:15 -0300

    commit update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,31 @@
 const nav_toggle = document.querySelector('.nav-btn');
-const nav = document.querySelector(".nav__links-container");
+const nav_links = document.querySelector(".nav__links-container");
+const nav = document.querySelector('.nav-bar');
+const logos = document.querySelectorAll('.logo');
 nav_toggle.addEventListener('click', (e) =>{
     e.preventDefault();
-    nav.classList.toggle('collapsed');
+    nav_links.classList.toggle('collapsed');
     nav_toggle.classList.toggle("clicked"); 
+});
+
+if(window.innerWidth >= 992){
+    logos[0].classList.add('--hide');
+    logos[1].classList.remove('--hide');
+}
+
+let scrollPos = window.scrollY;
+window.addEventListener('scroll', ()=>{
+    if(window.innerWidth < 992)
+        return false;
+    scrollPos = window.scrollY;
+    if (scrollPos >= 30) {
+        nav.classList.add('nav--scrolled');
+        logos[1].classList.add('--hide');
+        logos[0].classList.remove('--hide');
+    } else {
+        nav.classList.remove('nav--scrolled');
+        logos[1].classList.remove('--hide');
+        logos[0].classList.add('--hide');
+    }
+
 });
