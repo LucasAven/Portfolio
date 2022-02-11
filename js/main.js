695a3e666123bdbb65207ada82c43ab429a3fcd0
Lucas <lavendanozarza@alu.frlp.utn.edu.ar>
2022-02-11 19:29:07 -0300

    commit Fix responsive issues

--- js/main.js
+++ js/main.js
@@ -1,38 +1,39 @@
-const nav_toggle = document.querySelector('.nav-btn');
+const nav_toggle = document.querySelector(".nav-btn");
 const nav_links = document.querySelector(".nav__links-container");
-const nav = document.querySelector('.nav-bar');
-const logos = document.querySelectorAll('.logo');
-nav_toggle.addEventListener('click', (e) =>{
-    e.preventDefault();
-    nav_links.classList.toggle('collapsed');
-    nav_toggle.classList.toggle("clicked"); 
+const nav = document.querySelector(".nav-bar");
+const logos = document.querySelectorAll(".logo");
+nav_toggle.addEventListener("click", (e) => {
+  e.preventDefault();
+  nav_links.classList.toggle("collapsed");
+  nav_toggle.classList.toggle("clicked");
 });
 
-if(window.innerWidth >= 992){
-    logos[0].classList.add('--hide');
-    logos[1].classList.remove('--hide');
+if (window.innerWidth >= 992) {
+  logos[0].classList.add("--hide");
+  logos[1].classList.remove("--hide");
 }
 
 let scrollPos = window.scrollY;
-window.addEventListener('scroll', ()=>{
-    if(window.innerWidth < 992)
-        return false;
-    scrollPos = window.scrollY;
-    if (scrollPos >= 30) {
-        nav.classList.add('nav--scrolled');
-        logos[1].classList.add('--hide');
-        logos[0].classList.remove('--hide');
-    } else {
-        nav.classList.remove('nav--scrolled');
-        logos[1].classList.remove('--hide');
-        logos[0].classList.add('--hide');
-    }
-
+window.addEventListener("scroll", () => {
+  if (window.innerWidth < 992) return false;
+  scrollPos = window.scrollY;
+  if (scrollPos >= 30) {
+    nav.classList.add("nav--scrolled");
+    logos[1].classList.add("--hide");
+    logos[0].classList.remove("--hide");
+  } else {
+    nav.classList.remove("nav--scrolled");
+    logos[1].classList.remove("--hide");
+    logos[0].classList.add("--hide");
+  }
 });
 
-document.querySelectorAll('a.smooth-scroll[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({behavior: 'smooth'});
-    });
+document.querySelectorAll('a.smooth-scroll[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document
+      .querySelector(this.getAttribute("href"))
+      .scrollIntoView({ behavior: "smooth" });
+    nav_links.classList.toggle("collapsed");
+  });
 });
